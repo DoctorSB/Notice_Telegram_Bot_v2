31fa0764510adf67882568430bcf1aaf85ff6241
Bulat Sabirov <60960885+DoctorSB@users.noreply.github.com>
2023-06-05 23:45:28 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,61 @@
+import asyncio
+import logging
+
+import betterlogging as bl
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
+
+from tgbot.config import load_config
+from tgbot.handlers.admin import admin_router
+from tgbot.handlers.echo import echo_router
+from tgbot.handlers.user import user_router
+from tgbot.middlewares.config import ConfigMiddleware
+from tgbot.services import broadcaster
+
+logger = logging.getLogger(__name__)
+log_level = logging.INFO
+bl.basic_colorized_config(level=log_level)
+
+
+async def on_startup(bot: Bot, admin_ids: list[int]):
+    await broadcaster.broadcast(bot, admin_ids, "Бот був запущений")
+
+
+def register_global_middlewares(dp: Dispatcher, config):
+    dp.message.outer_middleware(ConfigMiddleware(config))
+    dp.callback_query.outer_middleware(ConfigMiddleware(config))
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+    )
+    logger.info("Starting bot")
+    config = load_config(".env")
+    if config.tg_bot.use_redis:
+        storage = RedisStorage.from_url(config.redis.dsn(), key_builder=DefaultKeyBuilder(with_bot_id=True, with_destiny=True))
+    else:
+        storage = MemoryStorage()
+    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp = Dispatcher(storage=storage)
+
+    for router in [
+        admin_router,
+        user_router,
+        echo_router
+    ]:
+        dp.include_router(router)
+
+    register_global_middlewares(dp, config)
+
+    await on_startup(bot, config.tg_bot.admin_ids)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error("Бот був вимкнений!")
---
+++ tgbot/config.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from environs import Env
+
+
+@dataclass
+class DbConfig:
+    host: str
+    password: str
+    user: str
+    database: str
+    port: int = 5432
+
+    # For SQLAlchemy
+    # def construct_sqlalchemy_url(self, driver="asyncpg", host=None, port=None) -> str:
+    #     if not host:
+    #         host = self.host
+    #     if not port:
+    #         port = self.port
+    #     uri = URL.create(
+    #         drivername=f"postgresql+{driver}",
+    #         username=self.user,
+    #         password=self.password,
+    #         host=host,
+    #         port=port,
+    #         database=self.database,
+    #     )
+    #     return uri.render_as_string(hide_password=False)
+
+
+@dataclass
+class TgBot:
+    token: str
+    admin_ids: list[int]
+    use_redis: bool
+
+
+@dataclass
+class RedisConfig:
+    redis_pass: Optional[str]
+    redis_port: Optional[int]
+    redis_host: Optional[str]
+
+    def dsn(self) -> str:
+        if self.redis_pass:
+            return f"redis://:{self.redis_pass}@{self.redis_host}:{self.redis_port}/0"
+        else:
+            return f"redis://{self.redis_host}:{self.redis_port}/0"
+
+
+@dataclass
+class Miscellaneous:
+    other_params: str = None
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    misc: Miscellaneous
+    db: DbConfig = None
+    redis: RedisConfig = None
+
+
+def load_config(path: str = None) -> Config:
+    env = Env()
+    env.read_env(path)
+
+    return Config(
+        tg_bot=TgBot(
+            token=env.str("BOT_TOKEN"),
+            admin_ids=list(map(int, env.list("ADMINS"))),
+            use_redis=env.bool("USE_REDIS")
+        ),
+
+        # db=DbConfig(
+        #     host=env.str('DB_HOST'),
+        #     password=env.str('POSTGRES_PASSWORD'),
+        #     user=env.str('POSTGRES_USER'),
+        #     database=env.str('POSTGRES_DB'),
+        # ),
+
+        # redis=RedisConfig(
+        #     redis_pass=env.str("REDIS_PASSWORD"),
+        #     redis_port=env.int("REDIS_PORT"),
+        #     redis_host=env.str("REDIS_HOST"),
+        # ),
+
+        misc=Miscellaneous()
+    )

df48ae7a0f98c08ab71a596683599f2306982a0b
macbook <ikamil522@gmail.com>
2023-07-06 20:34:59 +0300

    style

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -25,35 +25,13 @@ checker = Checker()
 
 @admin_router.message(CommandStart())
 async def admin_start(message: Message):
-    await message.reply("Админка!", reply_markup=ReplyKeyboardRemove()) # type: ignore
-
-
-@admin_router.message(Command(commands="set_checker"))
-async def get_checker_id(message: Message, state: FSMContext): # type: ignore
-    await state.set_state(Admin.WAITING_FOR_ADD_CHECKER)
-    await message.answer("Введите id пользователя, которого хотите сделать проверяющим")
-
-
-@admin_router.message(Admin.WAITING_FOR_ADD_CHECKER)
-async def set_checker_id(message: Message, state: FSMContext):
-    checker.set_id(message.text)
-
-    append_json('data.json', 'checkers', checker.get_id())
-    await state.clear()
-    await message.answer("Вы сделали пользователя проверяющим")
+    await message.reply("Админка!", reply_markup=ReplyKeyboardRemove())
 
 
 @admin_router.message(Command(commands="remove_checker"))
-async def get_remove_checker_id(message: Message, state: FSMContext): # type: ignore
+async def get_remove_checker_id(message: Message, state: FSMContext):  # type: ignore
     await state.set_state(Admin.WAITING_FOR_REMOVE_CHECKER)
     await message.answer("Введите id пользователя, которого хотите сделать проверяющим")
-
-
-@admin_router.message(Admin.WAITING_FOR_REMOVE_CHECKER)
-async def remove_checker_id(message: Message, state: FSMContext):
-    remove_json('data.json', 'checkers', message.text)
-    await state.clear()
-    await message.answer("Вы удалили пользователя из проверяющих")
 
 
 @admin_router.message(Command(commands="create_task"))
@@ -87,7 +65,8 @@ async def create_task(message: Message, state: FSMContext):
     quest.set_time_limit(str(date))
     quest.set_checker_id(message.from_user.id)
     await state.clear()
-    add_task(quest.get_quest_name(), quest.get_quest_description(), quest.get_quest_status(), quest.get_time_limit(), quest.get_checker_id())
+    add_task(quest.get_quest_name(), quest.get_quest_description(
+    ), quest.get_quest_status(), quest.get_time_limit(), quest.get_checker_id())
     await message.answer(f"Задание создано")
 
 
@@ -96,13 +75,13 @@ async def add_executor(message: Message, state: FSMContext):
     await message.answer("Выберете задание для добавления исполнителя", reply_markup=task_preview_keyboard('tasks.json'))
     await state.set_state(Admin.WAITING_FOR_TASK_ID)
 
+
 @admin_router.callback_query(Admin.WAITING_FOR_TASK_ID)
 async def get_task_id(query, state: FSMContext):
-    tasks = json_keys('tasks.json')
-    if query.data in tasks:
-        await state.update_data(task_id=query.data)
-        await query.message.edit_text(f"Перешлите сообщение от исполнителя")
-        await state.set_state(Admin.WAITING_FOR_ADD_EXECUTOR)
+    update_task_worker_list(query.data, query.from_user.id)
+    await query.message.edit_text(f"Исполнитель добавлен")
+    await state.set_state(Admin.WAITING_FOR_ADD_EXECUTOR)
+
 
 @admin_router.message(Admin.WAITING_FOR_ADD_EXECUTOR)
 async def add_executor_id(message: Message, state: FSMContext):
